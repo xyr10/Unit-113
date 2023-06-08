e9e66ef6fc254a15a4c881eaf12e1dee3dcc2eee
Xyrone Ocampo <xyrone@gmail.com>
2023-06-08 16:37:31 -0700

    Added draft support

---
+++ mc4.md
@@ -0,0 +1,19 @@
+# Mini challenge 4
+
+ 
+
+Update PostListView so that it only shows `Post` entries that are set to `published`.
+
+ 
+
+### Steps
+
+1. Update the PostListView class to override the default get_context_object method.
+
+2. Add any necessary filters to ensure that only published posts are visible on the page.
+
+3. Ensure that posts appear in descending order on the page.
+
+### Note
+
+It is recommended to base ourselves off of the get_context_data method in `DraftPostListView`.
--- posts/views.py
+++ posts/views.py
@@ -10,13 +10,13 @@ from django.contrib.auth.mixins import (
     LoginRequiredMixin,
     UserPassesTestMixin
 )
-from .models import Post
+from .models import Post, Status
 
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     template_name = 'posts/new.html'
     model = Post
-    fields = ['title', 'subtitle', 'body']
+    fields = ['title', 'subtitle', 'body', 'status']
               
     def form_valid(self, form):
         form.instance.author = self.request.user 
@@ -29,7 +29,7 @@ class PostDetailView(DetailView):
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     template_name = 'posts/edit.html'
     model = Post
-    fields = ['title', 'subtitle', 'body']
+    fields = ['title', 'subtitle', 'body', 'status']
 
     def test_func(self):
         post = self.get_object()
@@ -48,15 +48,25 @@ class PostListView(ListView):
     template_name = 'posts/list.html'
     model = Post
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        published_status = Status.objects.get(name='published')
+        context["post_list"] = Post.objects.filter(
+                status=published_status
+            ).order_by("created_on").reverse()
+        return context
 
 class DraftPostListView(LoginRequiredMixin, ListView):
-    template_name = "post/list.html"
+    template_name = "posts/list.html"
     model = Post
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        draft_status = Status.objects.get(name='draft')
         context["post_list"] = Post.objects.filter(
             author=self.request.user
+            ).filter(
+                status=draft_status
             ).order_by("created_on").reverse()
         return context
     
